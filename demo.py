d62f8d2c5971497891c83c226d6d1a38d3bc8212
Aboubacar OUATTARA <abouba181@gmail.com>
2021-12-13 02:13:13 -0500

    wip

--- demo.py
+++ demo.py
@@ -1,17 +1,19 @@
 import argparse
 from backend.camera import Camera, VideoPlayer
+from models.classification.custom import DetectVGG16FER
 from pipelines import DetectMiniXceptionFER
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Real-time face classifier')
-    parser.add_argument('-c', '--camera_id', type=int, default=0,
+    parser.add_argument('-c', '--camera_id', type=int, default=1,
                         help='Camera device ID')
     parser.add_argument('-o', '--offset', type=float, default=0.1,
                         help='Scaled offset to be added to bounding boxes')
     args = parser.parse_args()
 
-    pipeline = DetectMiniXceptionFER([args.offset, args.offset])
+    # pipeline = DetectMiniXceptionFER([args.offset, args.offset])
+    pipeline = DetectVGG16FER([args.offset, args.offset])
     camera = Camera(args.camera_id)
     player = VideoPlayer((640, 480), pipeline, camera)
     player.run()
---
+++ models/classification/custom.py
@@ -0,0 +1,25 @@
+import os.path
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.utils import get_file
+from pipelines import DetectMiniXceptionFER, MiniXceptionFER
+
+
+class DetectVGG16FER(DetectMiniXceptionFER):
+    def __init__(self, offsets=[0, 0], colors=None):
+        super().__init__(offsets=offsets, colors=colors)
+        self.classify = VGG16FER()
+
+
+class VGG16FER(MiniXceptionFER):
+    def __init__(self):
+        super().__init__()
+        self.classifier = VGG16((48, 48, 1), 7, weights='FER')
+
+
+def VGG16(input_shape, num_classes, weights=None):
+    filename = os.path.dirname(__file__) + '/vgg16_0_5887.h5'
+    # path = get_file(filename, filename, cache_subdir='paz/models')
+    model = load_model(filename)
+    model._name = 'MINI-XCEPTION'
+    return model
